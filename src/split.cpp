67244890558d9ef5cda2266e8a68d64d68ee5693
lva98 <leonardovalerio@live.com>
2023-11-10 09:09:26 -0300

    add pre_termo base

--- src/split.cpp
+++ src/split.cpp
@@ -7,52 +7,6 @@
 #include "rapidcsv.h"
 #include <map>
 
-// Função para dividir os dados mantendo a proporção dos rótulos
-// void splitData(const std::string &inputFile, double p1, double p2, double p3) {
-//     rapidcsv::Document doc(inputFile, rapidcsv::LabelParams(-1, -1));
-
-//     std::vector<std::vector<std::string>> data;
-//     std::vector<std::vector<std::string>> dataRemaining;
-
-//     // Lê os dados do arquivo CSV
-//     for (int i = 0; i < doc.GetRowCount(); ++i) {
-//         std::vector<std::string> row;
-//         for (int j = 0; j < doc.GetColumnCount(); ++j) {
-//             row.push_back(doc.GetCell<std::string>(j, i));
-//         }
-//         data.push_back(row);
-//     }
-
-//     // Ordena os dados com base nos rótulos (última coluna)
-//     std::sort(data.begin(), data.end(), [](const std::vector<std::string> &a, const std::vector<std::string> &b) {
-//         return a.back() < b.back();
-//     });
-
-//     // Separa os dados mantendo a proporção dos rótulos
-//     int pos1 = data.size() * p1;
-//     int pos2 = data.size() * (p1 + p2);
-
-//     std::vector<std::vector<std::string>> firstSet(data.begin(), data.begin() + pos1);
-//     std::vector<std::vector<std::string>> secondSet(data.begin() + pos1, data.begin() + pos2);
-//     std::vector<std::vector<std::string>> thirdSet(data.begin() + pos2, data.end());
-
-//     // Escreve os dados nos arquivos de saída
-//     rapidcsv::Document output1(inputFile + "_set1.csv", rapidcsv::LabelParams(-1, -1));
-//     rapidcsv::Document output2(inputFile + "_set2.csv", rapidcsv::LabelParams(-1, -1));
-//     rapidcsv::Document output3(inputFile + "_set3.csv", rapidcsv::LabelParams(-1, -1));
-
-//     for (const auto &row : firstSet) {
-//       output1.AddRow(row);
-//     }
-
-//     for (const auto &row : secondSet) {
-//         output2.AddRow(row);
-//     }
-
-//     for (const auto &row : thirdSet) {
-//         output3.AddRow(row);
-//     }
-// }
 using namespace std;
 
 void save_to_file (vector<vector<string>> &vec, string name) {
